445058bd5d92e3ce9bfd57c19854c79f5fdacfbe
dustydog10 <meaderoberts@comcast.net>
2016-05-23 15:47:52 -0700

    Added all edits and HTML for 6-20.

--- krug_ess4e/fix_links.pl
+++ krug_ess4e/fix_links.pl
@@ -19,6 +19,17 @@ while ($temp_line = <STDIN>) {
 
 $_ = $contents;
 
+# get chapter number from first line
+m`<chapter number="(\d+)"`;
+my $chapter = $1;
+
+# change the Krugman images with src="images" to src="asset/ch<CHAPTER>"
+s`src="images/Krug`src="asset/ch${chapter}/Krug`g;
+
+# change other to book level images directory
+s`src="images/`src="asset/images/`g;
+
+
 ###################################
 # fix inline chapter links
 
@@ -27,9 +38,20 @@ s`(<link href="krugmanwellsessentials4e)-ch\d+\.xml(">Chapter (\d+)<\/link>)`$1_
 ###################################
 # fix inline figure links
 
-s`(<link href=")#krugmanwellsessentials4e-ch\d+-fig-\d+(">Figure (\d+)-(\d+)<\/link>)`${1}asset/ch${3}/KrugESS4e_fig_${3}_${4}.html$2`g;
+s`(<link href=")#krugmanwellsessentials4e-ch\d+-fig-\d+(">Figure (\d+)-(\d+)<\/link>)`${1}asset/ch${3}/KrugESS4e_fig_${3}_${4}.html${2}`g;
+# caption links
+s`(<caption><link href=")#krugmanwellsessentials4e-ch\d+-fig-\d+(">.*?<phrase block_type="fig_num">(\d+)-(\d+))`${1}asset/ch${3}/KrugESS4e_fig_${3}_${4}.html${2}`g;
 # also fix zero padding for figure numbers less than 10
 s`(KrugESS4e_fig_\d+_)(\d)(\.html)`${1}0${2}${3}`g;
+
+###################################
+# fix table links
+
+s`(<link href=")#krugmanwellsessentials4e-ch(\d+)-tab-(\d+)`${1}asset/ch${2}/table_${2}_${3}.html`g;
+# numbered tables
+s`(data-href=")#krugmanwellsessentials4e-ch(\d+)-tab-(\d+)`${1}asset/ch${2}/table_${2}_${3}.html`g;
+# unnumbered tables
+s`(data-href=")#krugmanwellsessentials4e-ch(\d+)-untab-(\d+)`${1}asset/ch${2}/un_table_${2}_${3}.html`g;
 
 ###################################
 # output contents to STDOUT
